0c6adedc64c0f02fcf7deb41767489bc2be1f644
Drew Moseley <drew.moseley@northern.tech>
2018-08-16 20:50:52 +0000

    Use full path to python source and roots.pem file
    Signed-off-by: Drew Moseley <drew.moseley@northern.tech>

--- image/meta-gcp-iot/recipes-gcp/python-docs-samples/files/start-mqtt-example.sh
+++ image/meta-gcp-iot/recipes-gcp/python-docs-samples/files/start-mqtt-example.sh
@@ -7,9 +7,10 @@ if [ ! -e /opt/gcp/etc/gcp-config.sh ]; then
 fi
 
 source /opt/gcp/etc/gcp-config.sh
-python cloudiot_mqtt_example.py \
+python /opt/gcp/usr/bin/cloudiot_mqtt_example.py \
        --project_id "${PROJECT_ID}" \
        --registry_id "${REGISTRY_ID}" \
        --device_id "${DEVICE_ID}" \
        --algorithm RS256 \
+       --ca_certs /opt/gcp/etc/roots.pem \
        --private_key_file=/var/lib/mender/mender-agent.pem
